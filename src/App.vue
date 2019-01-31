35e61e41f605e193c7c1a936282e1904e5cd4c67
Alireza Mohsenii <alireza.mohsenikabir@gmail.com>
2019-01-31 15:32:38 +0330

    refactor(liteWeather):  make styles scoped

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
 <template>
   <div :class="bg">
       <div :class="period">
-
         <Header />
         <Middle />
         <Footer />
-        
       </div>
   </div>
 </template>
@@ -63,13 +61,8 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 @import url('https://fonts.googleapis.com/css?family=Open+Sans');
-
-* {
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
 
 body {
   margin: 0
@@ -112,77 +105,4 @@ body {
   padding-top: 10px;
 }
 
-.city {
-  font-family: Segoe UI;
-  font-size: 30px;
-  text-align: center;
-  color: white;
-}
-
-.date {
-  color: white;
-  font-family: roboto;
-  font-weight: 100;
-  text-align: center;
-  font-size: 20px;
-}
-
-
-.des {
-  color: white;
-  font-family: Segoe UI;
-  text-align: center;
-  font-size: 27px;
-}
-
-.des:first-letter {
-  text-transform: uppercase;
-}
-
-.more {
-  display: flex;
-}
-
-.Bottom {
-    width: 100%;
-    padding-top: 5px;
-    display: flex;
-    justify-content: space-evenly;
-}
-
-.left {
-  font-family: Segoe UI;
-  font-size: 15px;
-  color: rgba(255, 255, 255, 0.75);
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-}
-
-.left img {
-  vertical-align: middle;
-}
-
-.mid {
-  color: rgba(255, 255, 255, 0.75);
-}
-
-.mid img {
-  width: 100px;
-  height: 100px;
-  vertical-align: middle;
-}
-
-.right {
-  font-family: Segoe UI;
-  font-size: 15px;
-  color: rgba(255, 255, 255, 0.75);
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-}
-
-.right img {
-  vertical-align: middle;  
-}
 </style>
