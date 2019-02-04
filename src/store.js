6cd8e6346d28120337add58955f4c7289cc6d89f
Reza Ghanbari <mail.rezaghanbari@gmail.com>
2019-02-04 11:47:47 +0330

    Debug weather info request api

--- src/store.js
+++ src/store.js
@@ -1,90 +1,94 @@
 // modules
-import Vue from 'vue'
-import Vuex from 'vuex'
-import axios from 'axios'
+import Vue from "vue";
+import Vuex from "vuex";
+import axios from "axios";
 // helper
-import { getTime } from './helper/time.helper'
+import { getTime } from "./helper/time.helper";
 
-
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    currentTemp: '',
-    minTemp: '',
-    maxTemp:'',
-    sunrise: '',
-    sunset: '',
-    humidity: '',
-    wind: '',
-    overcast: '',
-    icon: '',
-    city: '',
-    country: '',
-    day: '',
-    month: '',
-    date: new Date(),
+    currentTemp: "",
+    minTemp: "",
+    maxTemp: "",
+    sunrise: "",
+    sunset: "",
+    humidity: "",
+    wind: "",
+    overcast: "",
+    icon: "",
+    city: "",
+    country: "",
+    day: "",
+    month: "",
+    date: new Date()
   },
 
   mutations: {
     changeDay(state, day) {
-      state.day = day
+      state.day = day;
     },
     changeMonth(state, month) {
-      state.month = month
+      state.month = month;
     },
     changeCurrentTemp(state, currentTemp) {
-      state.currentTemp = currentTemp
+      state.currentTemp = currentTemp;
     },
     changeMinTemp(state, minTemp) {
-      state.minTemp = minTemp
+      state.minTemp = minTemp;
     },
     changeMaxTemp(state, maxTemp) {
-      state.maxTemp = maxTemp
+      state.maxTemp = maxTemp;
     },
     changeHumidity(state, humidity) {
-      state.humidity = humidity
+      state.humidity = humidity;
     },
     changeWind(state, wind) {
-      state.wind = wind
+      state.wind = wind;
     },
     changeOvercast(state, overcast) {
-      state.overcast = overcast
+      state.overcast = overcast;
     },
     changeIcon(state, icon) {
-      state.icon = icon
+      state.icon = icon;
     },
     changeSunrise(state, sunrise) {
-      state.sunrise = sunrise
+      state.sunrise = sunrise;
     },
     changeSunset(state, sunset) {
-      state.sunset = sunset
+      state.sunset = sunset;
     },
     changeCity(state, city) {
-      state.city = city
+      state.city = city;
     },
     changeCountry(state, country) {
-      state.country = country
-    },
+      state.country = country;
+    }
   },
   actions: {
     getWeather({ commit } /* , url */) {
       axios
-        .get('http://api.openweathermap.org/data/2.5/weather?q=tehran&units=metric&APPID=390e4e6a63e039237f1b345548b99954')
+        .get(
+          "https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?q=tehran&units=metric&APPID=390e4e6a63e039237f1b345548b99954"
+        )
         .then(({ data: { main, sys, wind, weather, name } }) => {
-          commit('changeCurrentTemp', Math.floor(main.temp))
-          commit('changeMinTemp', main.temp_min)
-          commit('changeMaxTemp', main.temp_max)
-          commit('changeHumidity', main.humidity + '%')
-          commit('changeWind', wind.speed + 'm/s')
-          commit('changeOvercast', weather[0].description)
-          commit('changeIcon', require("./assets/" + weather[0].icon.slice(0, 2) + ".svg"))
-          commit('changeSunrise', getTime(sys.sunrise*1000))
-          commit('changeSunset', getTime(sys.sunset*1000))
-          commit('changeCity', name)
-          commit('changeCountry', sys.country)
-      })
-      .catch(console.log)
+          commit("changeCurrentTemp", Math.floor(main.temp));
+          commit("changeMinTemp", main.temp_min);
+          commit("changeMaxTemp", main.temp_max);
+          commit("changeHumidity", main.humidity + "%");
+          commit("changeWind", wind.speed + "m/s");
+          commit("changeOvercast", weather[0].description);
+          commit(
+            "changeIcon",
+            require("./assets/" + weather[0].icon.slice(0, 2) + ".svg")
+          );
+          commit("changeSunrise", getTime(sys.sunrise * 1000));
+          commit("changeSunset", getTime(sys.sunset * 1000));
+          commit("changeCity", name);
+          commit("changeCountry", sys.country);
+        })
+        .catch(console.log);
     }
   }
-})
+});
